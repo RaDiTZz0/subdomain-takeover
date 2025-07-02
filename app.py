95aafbc7444eb50a32c7beed4228de9c186fff05
Xande Araújo (RaDiTZz) <85699023+RaDiTZz0@users.noreply.github.com>
2025-07-02 09:37:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def show_readme():
-    url = "https://raw.githubusercontent.com/RaDiTZz0/subdomain-takeover/refs/heads/main/README.md"
+    url = "https://raw.githubusercontent.com/RaDiTZz0/subdomain-takeover/refs/heads/main/takeover_poc.html"
     r = requests.get(url)
     if r.status_code == 200:
-        return Response(r.text, mimetype='text/plain')
+        return Response(r.text, mimetype='text/html')
     else:
-        return f"Erro ao carregar README.md: {r.status_code}"
+        return f"Erro ao carregar takeover_poc.html: {r.status_code}"
